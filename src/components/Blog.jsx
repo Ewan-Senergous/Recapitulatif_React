cffa5dd240433843e74b1073f9546cd246a07294
Your Name <ewan16270409@outlook.fr>
2024-07-21 10:39:38 +0200

    Props AND PropTypes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,19 @@
-function Blog() {
+import PropTypes from "prop-types";
+function Blog({ science }) {
   return (
     <div>
       <h1>Hello React</h1>
+      <p>{science.title}</p>
+      <p>{science.description}</p>
     </div>
   );
 }
+
+Blog.propTypes = {
+  science: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+  }).isRequired,
+};
+
 export default Blog;
